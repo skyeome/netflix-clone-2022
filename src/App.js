ffea60c9c1352bb385ac670321b6ee012e1a7e77
skyeome <tjru7230@naver.com>
2022-04-20 15:25:40 +0900

    #1.3 animation and pseudo selectors

--- src/App.js
+++ src/App.js
@@ -1,32 +1,42 @@
-import styled from "styled-components";
+import styled,{ keyframes } from "styled-components";
 
 const Wrapper = styled.div`
   display:flex;
 `;
-const Btn = styled.button`
-  color: #fff;
-  background-color: tomato;
-  padding:10px;
-  border:0;
-  border-radius: 15px;
+const ani = keyframes`
+  0%{
+    transform: rotate(0);
+  }
+  100%{
+    transform: rotate(360deg);
+  }
 `;
-const Input = styled.input.attrs({type:"text",required:true})`
-  border:1px solid #efefef;
-  border-radius: 5px;
+const Box = styled.div`
+  width:200px;
+  height:200px;
+  background-color: tomato;
+  animation: ${ani} 2s linear infinite;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  span{
+    font-size: 40px;
+    &:hover{
+      font-size: 50px;
+    }
+    &:active{
+      opacity: 0;
+    }
+  }
 `;
 
 function App() {
   return (
     <div className="App">
       <Wrapper>
-        <Btn as="a" href="/login">Log in</Btn>
-        <Btn>Log out</Btn>
-      </Wrapper>
-      <Wrapper>
-        <Input />
-        <Input />
-        <Input />
-        <Input />
+        <Box>
+          <span>😃</span>
+        </Box>
       </Wrapper>
     </div>
   );
