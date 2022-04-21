49766f932c267724aaba29780164a4f7a794a713
skyeome <tjru7230@naver.com>
2022-04-21 10:44:54 +0900

    #2.1 Typing the Props

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+import Circle from "./Circle";
+
+function App() {
+  return (
+    <div className="App">
+      <Circle bgColor="teal" />
+      <Circle bgColor="tomato" />
+    </div>
+  );
+}
+
+export default App;
